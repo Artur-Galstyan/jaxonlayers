afdad259cd0d7e52a63b1ed283d831901d295e6b
Artur Galstyan <galstyan.artu@gmail.com>
2025-11-23 21:13:24 +0100

    expose more layers in init

--- jaxonlayers/layers/__init__.py
+++ jaxonlayers/layers/__init__.py
@@ -1,7 +1,9 @@
+from .abstract import AbstractNorm, AbstractNormStateful
 from .attention import MultiheadAttention, SqueezeExcitation
 from .convolution import ConvNormActivation
 from .normalization import BatchNorm, LayerNorm, LocalResponseNormalization
 from .regularization import StochasticDepth
+from .sequential import BatchedLinear
 from .state_space import SelectiveStateSpace
 
 __all__ = [
@@ -13,4 +15,7 @@ __all__ = [
     "StochasticDepth",
     "ConvNormActivation",
     "LayerNorm",
+    "AbstractNormStateful",
+    "AbstractNorm",
+    "BatchedLinear",
 ]
