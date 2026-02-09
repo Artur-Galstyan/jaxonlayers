68e61472aadb45edc7c013500d89ebef29f571c7
Artur Galstyan <galstyan.artu@gmail.com>
2026-02-09 19:11:00 +0100

    dtype to sinusoidal embs

--- jaxonlayers/functions/embedding.py
+++ jaxonlayers/functions/embedding.py
@@ -1,18 +1,22 @@
 import jax.numpy as jnp
+from beartype.typing import Any
 from jaxtyping import Array, Float, Int
+
+from jaxonlayers.functions import default_floating_dtype
 
 
 def sinusoidal_embedding(
-    t: Int[Array, ""], embedding_size: int
+    t: Int[Array, ""], embedding_size: int, dtype: Any | None = None
 ) -> Float[Array, " embedding_size"]:
+    if dtype is None:
+        dtype = default_floating_dtype()
+    assert dtype is not None
     if embedding_size % 2 != 0:
         raise ValueError(f"Embedding size must be even, but got {embedding_size}")
 
     half_dim = embedding_size // 2
     embedding_freqs = jnp.exp(
-        -jnp.log(10000)
-        * jnp.arange(start=0, stop=half_dim, dtype=jnp.float32)
-        / half_dim
+        -jnp.log(10000) * jnp.arange(start=0, stop=half_dim, dtype=dtype) / half_dim
     )
 
     time_args = t * embedding_freqs
